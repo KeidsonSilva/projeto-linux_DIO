5df6edb6888020a7362110b057aec7869597b2eb
KeidsonSilva <keidsonsilva6@gmail.com>
2022-08-17 22:34:14 -0300

    Arquivo de texto shell script versão 1

---
+++ iac1.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+echo "***********************"
+echo "Criando os diretórios: "
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "***********************"
+echo "Criando os grupos de usuários: "
+groupadd GRP_ADM
+groupadd GPR_VEN
+groupadd GRP_SEC
+
+echo "***********************"
+echo "Criando os usuários:"
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "******************"
+echo "especificando os donos dos diretórios"
+
+chown root:GRP_ADM /adm
+chown root:GPR_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "fim"
+
